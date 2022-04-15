08b1902554e6e590be42e2b77be7cabdfae01bbc
Katie Kim <kak010@ucsd.edu>
2022-04-15 16:14:53 -0700

    Corrected if there is incorrect syntax when calling a link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -23,8 +23,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(markdown.indexOf("[", openBracket+1) > closeParen || markdown.indexOf("[", openBracket+1) < 0){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            else{
+                currentIndex = markdown.indexOf("[", openBracket+1);
+            }
         }
 
         return toReturn;
