2bdc589ed5c9d183c1d4862d9aa205504afaad49
cs15lsp22ahg <cttang@ucsd.edu>
2022-04-22 14:20:10 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,35 +11,34 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            if(openBracket > 0 && markdown.charAt(openBracket-1)=='!'){
-                openBracket = markdown.indexOf("[", openBracket+1);
+            if (openBracket > 0 && markdown.charAt(openBracket - 1) == '!') {
+                openBracket = markdown.indexOf("[", openBracket + 1);
             }
             if (openBracket < 0) {
                 return toReturn;
             }
-            
+
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.indexOf("[", openBracket+1) > closeParen || markdown.indexOf("[", openBracket+1) < 0){
+            if (markdown.indexOf("[", openBracket + 1) > closeParen || markdown.indexOf("[", openBracket + 1) < 0) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
-            }
-            else{
-                currentIndex = markdown.indexOf("[", openBracket+1);
+            } else {
+                currentIndex = markdown.indexOf("[", openBracket + 1);
             }
         }
 
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
+        System.out.println("hi");
     }
 }
