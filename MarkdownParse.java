342178b544fc394359b7ad480cd71a03e400ee6e
cs15lsp22ahg <cttang@ucsd.edu>
2022-05-06 14:35:53 -0700

    edit MarkdownParse.java with commentto test Makefile

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-
+    // add comment to test Makefile
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (,
